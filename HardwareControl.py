1d0767833f5311a672cc141917d91111be09423e
KIMDOHYEONG <32589504+CPUDoHyeong@users.noreply.github.com>
2019-03-11 12:41:15 +0900

    Update HardwareControl.py

--- HardwareControl.py
+++ HardwareControl.py
@@ -2,6 +2,7 @@
 This is an IOT project using MQTT protocol.
 This was created to control the boiler with a smartphone.
 Use AWS, MQTT protocol, android studio, Raspberry Pi.
+ttttt
 '''
 # import
 import paho.mqtt.client as mqtt
